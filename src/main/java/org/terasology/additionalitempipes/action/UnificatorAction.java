beaeb8911cbb8de6eba2d3210bd0ec1127e13b66
Tobias Nett <skaldarnar@googlemail.com>
2021-02-06 20:14:08 +0100

    feat(JOML): migrate `BlockComponent` (#21)
    Depends on MovingBlocks/Terasology#4470

--- src/main/java/org/terasology/additionalitempipes/action/UnificatorAction.java
+++ src/main/java/org/terasology/additionalitempipes/action/UnificatorAction.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.additionalitempipes.action;
 
-import org.joml.Vector3i;
+import org.joml.Vector3ic;
 import org.terasology.additionalitempipes.components.UnificatorComponent;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.EventPriority;
@@ -29,7 +16,6 @@ import org.terasology.itempipes.event.PipeInsertEvent;
 import org.terasology.logic.common.ActivateEvent;
 import org.terasology.logic.common.lifespan.LifespanComponent;
 import org.terasology.logic.inventory.PickupComponent;
-import org.terasology.math.JomlUtil;
 import org.terasology.math.Side;
 import org.terasology.physics.components.RigidBodyComponent;
 import org.terasology.registry.In;
@@ -37,6 +23,7 @@ import org.terasology.world.WorldProvider;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockComponent;
 import org.terasology.world.block.BlockManager;
+
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -59,7 +46,7 @@ public class UnificatorAction extends BaseComponentSystem {
             event.consume();
             return;
         }
-        Vector3i posi = JomlUtil.from(event.getTarget().getComponent(BlockComponent.class).position);
+        Vector3ic posi = event.getTarget().getComponent(BlockComponent.class).getPosition();
         Side side = Side.inDirection(event.getHitNormal());
 
         switch (side) {
@@ -102,7 +89,7 @@ public class UnificatorAction extends BaseComponentSystem {
         item.removeComponent(RigidBodyComponent.class);
         item.removeComponent(LifespanComponent.class);
         item.removeComponent(PickupComponent.class);
-        Vector3i position = JomlUtil.from(block.position);
+        Vector3ic position = block.getPosition();
         if (outputside != null) {
             int num = 0;
             switch (outputside) {
@@ -130,7 +117,7 @@ public class UnificatorAction extends BaseComponentSystem {
         }
     }
 
-    private void inputOrDrop(EntityRef item, Vector3i position, Side side) {
+    private void inputOrDrop(EntityRef item, Vector3ic position, Side side) {
         Map<Side, EntityRef> pipes = pipeSystem.findPipes(position);
         EntityRef pipe = pipes.get(side);
         if (pipe != null) {
