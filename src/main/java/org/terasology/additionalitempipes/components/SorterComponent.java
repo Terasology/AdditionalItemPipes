0f8ebe406e290f40ca8efe638fb19a899be7a25e
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 20:01:08 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#24)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/additionalitempipes/components/SorterComponent.java
+++ src/main/java/org/terasology/additionalitempipes/components/SorterComponent.java
@@ -1,15 +1,20 @@
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
 package org.terasology.additionalitempipes.components;
 
-import org.terasology.engine.entitySystem.Component;
+import com.google.common.collect.Lists;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This Component differentiates the Sorter from other blocks. It contains the Sorter's filter and index od the side to which items not listed in the filter will be put.
  */
-public class SorterComponent implements Component {
+public class SorterComponent implements Component<SorterComponent> {
     //A list of 6 lists (one per side) of strings (strings for filtering purposes based on the item's data)
     public List<List<String>> filter = new LinkedList<List<String>>() {{
         add(new ArrayList<>());
@@ -21,4 +26,12 @@ public class SorterComponent implements Component {
     }};
 
     public int defaultSideNum = 0;
+
+    @Override
+    public void copyFrom(SorterComponent other) {
+        this.filter = other.filter.stream()
+                .map(Lists::newArrayList)
+                .collect(Collectors.toList());
+        this.defaultSideNum = other.defaultSideNum;
+    }
 }
